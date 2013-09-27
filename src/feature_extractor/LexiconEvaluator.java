a224e4bfc51180113c4bb3703090982779bac19f
fbravo <felipebravom@gmail.com>
2013-09-27 16:32:00 -0300

    otros lexicos

--- src/feature_extractor/LexiconEvaluator.java
+++ src/feature_extractor/LexiconEvaluator.java
@@ -58,23 +58,44 @@ public class LexiconEvaluator {
     }
 
     static public void main(String args[]) {
-        LexiconEvaluator l = new LexiconEvaluator("extra/polarity-lexicon.txt");
-        l.processDict();
-        System.out.println(l.retrieveValue("wrong"));
-        System.out.println(l.retrieveValue("happy"));
-        System.out.println(l.retrieveValue("good"));
-
-
-        LexiconEvaluator l2 = new LexiconEvaluator("extra/AFINN-111.txt");
+//        LexiconEvaluator l = new LexiconEvaluator("extra/polarity-lexicon.txt");
+//        l.processDict();
+//        System.out.println(l.retrieveValue("wrong"));
+//        System.out.println(l.retrieveValue("happy"));
+//        System.out.println(l.retrieveValue("good"));
+//
+//
+//        LexiconEvaluator l2 = new LexiconEvaluator("extra/AFINN-111.txt");
+//        l2.processDict();
+//        System.out.println(l2.retrieveValue("wrong"));
+//        System.out.println(l2.retrieveValue("happy"));
+//        System.out.println(l2.retrieveValue("good"));
+//                System.out.println(l2.retrieveValue(":-("));
+//        System.out.println(l2.retrieveValue(":)"));
+//        System.out.println(l2.retrieveValue(":("));
+        
+        
+        LexiconEvaluator l2 = new LexiconEvaluator("extra/Sentiment140-Lexicon-v0.1/unigrams-pmilexicon.txt");
         l2.processDict();
-        System.out.println(l2.retrieveValue("wrong"));
-        System.out.println(l2.retrieveValue("happy"));
-        System.out.println(l2.retrieveValue("good"));
-                System.out.println(l2.retrieveValue(":-("));
-        System.out.println(l2.retrieveValue(":)"));
-        System.out.println(l2.retrieveValue(":("));
+        System.out.println(l2.retrieveValue("love"));
+        System.out.println(l2.retrieveValue("hate"));
+        System.out.println(l2.retrieveValue("sick"));
+        
 
-
+        LexiconEvaluator l3 = new LexiconEvaluator("extra/NRC-Hashtag-Sentiment-Lexicon-v0.1/unigrams-pmilexicon.txt");
+        l3.processDict();
+        System.out.println(l3.retrieveValue("#love"));
+        System.out.println(l3.retrieveValue("#sad"));
+        System.out.println(l3.retrieveValue("#sick"));
+        
+        LexiconEvaluator l4 = new LexiconEvaluator("extra/BingLiu.csv");
+        l4.processDict();
+        System.out.println(l4.retrieveValue("love"));
+        System.out.println(l4.retrieveValue("hate"));
+        System.out.println(l4.retrieveValue("sick"));
+        
+        
+        
 
     }
 }
